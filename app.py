b92bf76b5cc998d029e2481e31218b43985244e7
Pankaj Datta <pankajdatta70058@gmail.com>
2026-05-18 08:58:54 +0530

    Updated Render port config

--- app.py
+++ app.py
@@ -144,5 +144,7 @@ def delete_student(id):
 # -----------------------
 # Run App
 # -----------------------
+import os
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
